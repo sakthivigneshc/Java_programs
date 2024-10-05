41ea5f772a08057b3d913e2214d1c1ae56931364
C <sakthivigneshc3@gmail.com>
2024-10-05 23:46:47 +0530

    Day_1_File_Added

---
+++ src/week1/String_Reverse.java
@@ -0,0 +1,18 @@
+package week1;
+
+public class String_Reverse {
+	
+	public static void main(String[] args) {
+		String  city= "America";
+		String reverseCity ="";
+		int wordLength = city.length();
+		System.out.println("The String before reversed : "+city);
+		
+		for(int i=wordLength-1; i>=0; i-- ) {
+			char ch = city.charAt(i);
+			reverseCity = reverseCity + ch;
+		}
+		System.out.println("The String after reversed : "+reverseCity);
+	}
+
+}
