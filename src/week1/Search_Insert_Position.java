8bfc3231bec22dc28e029ba91377bccac72ae4a1
C <sakthivigneshc3@gmail.com>
2024-10-11 08:24:04 +0530

    SearchInsertPosition_qn_soln

---
+++ src/week1/Search_Insert_Position.java
@@ -0,0 +1,33 @@
+package week1;
+
+public class Search_Insert_Position {
+	
+	    public static int searchInsert(int[] nums, int target) {
+	        int low = 0;
+	        int high = nums.length-1;
+
+	        while(low<=high){
+	            int mid = low+(high-low)/2;
+	            if(nums[mid] ==  target){
+	                return mid;
+	            }
+	            else if(nums[mid]> target){
+	                // low = mid+1;
+	                high = mid-1;
+	            }
+	            else{
+	             low = mid+1;   
+	            }
+	        }
+
+	        return low;
+
+
+	}
+	    public static void main(String[] args) {
+	    	int nums[] = {1,3,5,6};
+			int target =  2;
+			int output = searchInsert(nums , target);
+			System.out.println(output);
+		}
+}
