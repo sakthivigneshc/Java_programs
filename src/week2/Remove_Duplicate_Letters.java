265b407decb7b7bc89802eebcffd1b884cadd718
C <sakthivigneshc3@gmail.com>
2024-10-14 00:09:13 +0530

    RemoveDuplicateLetters.java

--- src/week2/Remove_Duplicate_Letters.java
+++ src/week2/Remove_Duplicate_Letters.java
@@ -1,31 +1,32 @@
 package week2;
 
-import java.util.HashSet;
-import java.util.Set;
+import java.util.LinkedHashSet;
 
 public class Remove_Duplicate_Letters {
 //	Remove Duplicate Letters
-	
-	
 	public static String removeDuplicateLetters(String s) {
+		char[] ch = s.toCharArray();
+		LinkedHashSet<Character> set = new LinkedHashSet<Character>();
+		StringBuilder builder  = new StringBuilder();
 
-        Set<Character> set = new HashSet<>();
+		for(int i=0; i<ch.length; i++)
+		{
+			
+			if(set.add(ch[i])) {
+	    		   builder.append(ch[i]);
+	    	   }
 
-//        char []  ch = s.toCharArray();
-        for(int i=0; i<=s.length()-1;i++) {
-            set.add(s.charAt(i));
-            }
-//        set.
-		return set.toString();
-        
+			
+		}
+		return builder.toString();     
     }
 	
 	public static void main(String[] args) {
-		String s  ="bcabc";
-  System.out.println(removeDuplicateLetters(s) );
+		String s  ="cbacdcbc";
+		System.out.println(removeDuplicateLetters(s) );
 	}
-	
-	
 	
 }
 
+
+//FAQ
