f9bc8c9efcc5e7a4472e321030c3228cf37fe111
C <sakthivigneshc3@gmail.com>
2024-10-21 23:29:21 +0530

    Is_ContainsDupilicateInArray_Soln

---
+++ src/part4/ContainsDuplicate.java
@@ -0,0 +1,36 @@
+package part4;
+
+import java.util.HashSet;
+
+public class ContainsDuplicate {
+	public static  boolean numArray(int[] numbers) {
+		
+		HashSet<Integer> resValues = new HashSet<Integer>();
+		for(int value : numbers ) {
+			if(resValues.contains(value)){
+				return true;
+			}
+			resValues.add(value);
+		}
+		return false;
+		
+	}
+	public static void main(String[] args) {
+		
+		int [] numbers  = { 1,2,3,1};
+		
+		if(numArray(numbers)) {
+			System.out.println("This array contains duplicate number.");
+		}
+		else {
+			System.out.println("This array not contains duplicate number.");
+		}
+
+		
+		
+	}
+	
+	
+	
+
+}
