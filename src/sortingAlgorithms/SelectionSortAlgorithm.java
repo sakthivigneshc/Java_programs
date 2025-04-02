300ccee1ef6e94da251f7dcb5a9d5e3204a8e313
C <sakthivigneshc3@gmail.com>
2025-04-02 08:04:16 +0530

    Selection_sort_Algo

---
+++ src/sortingAlgorithms/SelectionSortAlgorithm.java
@@ -0,0 +1,38 @@
+package sortingAlgorithms;
+
+import java.util.*;
+
+public class SelectionSortAlgorithm {
+	
+    static void selection_sort(int arr[], int n) {
+        for (int i = 0; i < n; i++) {
+            int mini = i;
+            for (int j = i + 1; j < n; j++) {
+                if (arr[j] < arr[mini]) {
+                    mini = j;
+                }
+            }
+            int temp = arr[mini];
+            arr[mini] = arr[i];
+            arr[i] = temp;
+        }
+
+        System.out.println("After selection sort:");
+        for (int i = 0; i < n; i++) {
+            System.out.print(arr[i] + " ");
+        }
+        System.out.println();
+    }
+
+    public static void main(String args[]) {
+
+        int arr[] = {3453, 89876, 294, 5200, 2009876, 9};
+        int n = arr.length;
+        System.out.println("Before selection sort:");
+        for (int i = 0; i < n; i++) {
+            System.out.print(arr[i] + " ");
+        }
+        System.out.println();
+        selection_sort(arr, n);
+    }
+}
